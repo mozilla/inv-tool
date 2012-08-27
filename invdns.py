10da6e524e6af3f3673c6c95d3cc724ce920591a
uberj <uberj@onid.orst.edu>
2012-08-27 21:06:49 +0100

    initial commit

---
+++ invdns.py
@@ -0,0 +1,182 @@
+import sys
+import argparse
+from options import *
+
+inv_parser = argparse.ArgumentParser(prog='invdns')
+inv_parser.add_argument('--format', default='text', type=str,
+            choices=['json', 'text', 'bind'],
+            help="The format the output")
+record_base_parser = inv_parser.add_subparsers(dest='rtype')
+
+def dns_command_template(rtype):
+    base_parser = record_base_parser.add_parser(rtype, help="The interface for {0} "
+                "records".format(rtype), add_help=True)
+    action_parser = base_parser.add_subparsers(help="{0} record "
+                "actions".format(rtype), dest='action')
+    create_parser = action_parser.add_parser('create', help="Create "
+                        "an {0} record".format(rtype))
+    update_parser = action_parser.add_parser('update', help="Update "
+                        "an {0} record".format(rtype))
+    delete_parser = action_parser.add_parser('delete', help="Delete "
+                        "an {0} record".format(rtype))
+    return action_parser, create_parser, update_parser, delete_parser
+
+# A, AAAA, PTR, CNAME, MX, NS, SRV, TXT, SSHFP and SOA
+
+# A Record
+(a_action_parser, a_create_parser, a_update_parser,
+        a_delete_parser) = dns_command_template('A')
+# Create A Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_ip_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(a_create_parser), args)
+
+# Update A Record options
+add_update_id_argument(a_update_parser, 'A')
+map(lambda apply_arg: apply_arg(a_update_parser, required=False), args)
+
+# Delete A Record options
+add_delete_id_argument(a_delete_parser, 'A')
+
+# AAAA Record
+(aaaa_action_parser, aaaa_create_parser, aaaa_update_parser,
+        aaaa_delete_parser) = dns_command_template('AAAA')
+
+# Create AAAA Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_ip_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(aaaa_create_parser), args)
+
+# Update AAAA Record options
+add_update_id_argument(aaaa_update_parser, 'AAAA')
+map(lambda apply_arg: apply_arg(aaaa_update_parser, required=False), args)
+
+# Delete AAAA Record options
+add_delete_id_argument(aaaa_delete_parser, 'AAAA')
+
+# PTR Record
+(ptr_action_parser, ptr_create_parser, ptr_update_parser,
+        ptr_delete_parser) = dns_command_template('PTR')
+
+# Create PTR Record options
+args = [add_ip_argument,
+        add_ttl_argument,
+        add_target_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(ptr_create_parser), args)
+
+# Update PTR Record options
+add_update_id_argument(ptr_update_parser, 'PTR')
+map(lambda apply_arg: apply_arg(ptr_update_parser, required=False), args)
+
+# Delete PTR Record options
+add_delete_id_argument(ptr_delete_parser, 'PTR')
+
+# CNAME Record
+(cname_action_parser, cname_create_parser, cname_update_parser,
+        cname_delete_parser) = dns_command_template('CNAME')
+
+# Create CNAME Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_target_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(cname_create_parser), args)
+
+# Update CNAME Record options
+add_update_id_argument(cname_update_parser, 'CNAME')
+map(lambda apply_arg: apply_arg(cname_update_parser, required=False), args)
+
+# Delete CNAME Record options
+add_delete_id_argument(cname_delete_parser, 'CNAME')
+
+# NS Record
+(ns_action_parser, ns_create_parser, ns_update_parser,
+        ns_delete_parser) = dns_command_template('NS')
+
+# Create NS Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_target_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(ns_create_parser), args)
+
+# Update NS Record options
+add_update_id_argument(ns_update_parser, 'NS')
+map(lambda apply_arg: apply_arg(ns_update_parser, required=False), args)
+
+# Delete NS Record options
+add_delete_id_argument(ns_delete_parser, 'NS')
+
+# MX Record
+(mx_action_parser, mx_create_parser, mx_update_parser,
+        mx_delete_parser) = dns_command_template('MX')
+
+# Create MX Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_priority_argument,
+        add_target_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(mx_create_parser), args)
+
+# Update MX Record options
+add_update_id_argument(mx_update_parser, 'MX')
+map(lambda apply_arg: apply_arg(mx_update_parser, required=False), args)
+
+# Delete MX Record options
+add_delete_id_argument(mx_delete_parser, 'MX')
+
+# SRV Record
+(srv_action_parser, srv_create_parser, srv_update_parser,
+        srv_delete_parser) = dns_command_template('SRV')
+
+# Create SRV Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_priority_argument,
+        add_port_argument,
+        add_weight_argument,
+        add_target_argument,
+        add_comment_argument]
+
+map(lambda apply_arg: apply_arg(srv_create_parser), args)
+
+# Update SRV Record options
+add_update_id_argument(srv_update_parser, 'SRV')
+map(lambda apply_arg: apply_arg(srv_update_parser, required=False), args)
+
+# Delete SRV Record options
+add_delete_id_argument(srv_delete_parser, 'SRV')
+
+# TXT Record
+(txt_action_parser, txt_create_parser, txt_update_parser,
+        txt_delete_parser) = dns_command_template('TXT')
+
+# Create TXT Record options
+args = [add_label_argument,
+        add_domain_argument,
+        add_ttl_argument,
+        add_text_argument,
+        add_comment_argument]
+map(lambda apply_arg: apply_arg(txt_create_parser), args)
+
+# Update TXT Record options
+add_update_id_argument(txt_update_parser, 'TXT')
+map(lambda apply_arg: apply_arg(txt_update_parser, required=False), args)
+
+# Delete TXT Record options
+add_delete_id_argument(txt_delete_parser, 'TXT')
+
+if __name__ == "__main__":
+    print inv_parser.parse_args(sys.argv[1:])
---
+++ options.py
@@ -0,0 +1,64 @@
+import argparse
+
+###############
+# DNS OPTIONS #
+###############
+
+def add_label_argument(parser, required=True):
+    parser.add_argument('--label', default="", type=str, dest='label',
+            help="The first label in the fqdn. If label is ommited then '' is "
+            "used and is analigouse to using '@' in a zone file (the record "
+            "will get it's domain's name as it's fqdn).", required=required)
+
+def add_domain_argument(parser, required=True):
+    parser.add_argument('--domain', default=None, type=str, dest='domain',
+            help="The domain a record is in.", required=required)
+
+def add_ip_argument(parser, required=True):
+    parser.add_argument('--ip', default=None, type=str, dest='ip', help="A "
+            "string representation of an IP address.", required=required)
+
+def add_target_argument(parser, required=True):
+    parser.add_argument('--target', default=None, type=str, dest='target',
+            help="The target name of a record", required=required)
+
+def add_comment_argument(parser, required=False):
+    parser.add_argument('--comment', default="", type=str, dest='comment',
+            help="Tell us a little about this record", required=False)
+
+def add_text_argument(parser, required=True):
+    parser.add_argument('--text', default=None, type=str, dest='text',
+            help="The text data.", required=False)
+
+def write_num_argument(parser, name, dest, help_text, required=False):
+    parser.add_argument('--{0}'.format(name), default=None, type=int,
+            dest=dest, help=help_text, required=required)
+    return parser
+
+def add_ttl_argument(parser, required=True):
+    write_num_argument(parser, 'ttl', 'ttl', "The ttl "
+                        "of a record.", required=False)
+
+def add_priority_argument(parser, required=True):
+    write_num_argument(parser, 'priority', 'priority',
+                        "The priority number of a record", required=required)
+
+def add_port_argument(parser, required=True):
+    write_num_argument(parser, 'port', 'port', "The "
+                        "target port of an SRV " "record", required=required)
+
+def add_weight_argument(parser, required=True):
+    write_num_argument(parser, 'weight', 'weight', "The "
+                        "weight number of an SRV record", required=required)
+
+def add_update_id_argument(parser, rtype):
+    parser.add_argument('--{0}'.format("pk"), required=True, default=None,
+            type=int, dest='pk', help="The database integer primary key (id) "
+            "of the {0} you are updating.".format(rtype))
+    return parser
+
+def add_delete_id_argument(parser, rtype):
+    parser.add_argument('--{0}'.format("pk"), default=None, type=int,
+            dest='pk', help="Delete the {0} record with the database primary "
+            "key of 'pk'".format(rtype), required=True)
+    return parser
