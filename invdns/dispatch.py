302550315d784ad5e2da4084605c6f00579e3d4d
uberj <uberj@onid.orst.edu>
2012-11-05 09:06:01 -0800

    instead of --format json and --format silent, you should now do --json or --silent

--- invdns/dispatch.py
+++ invdns/dispatch.py
@@ -29,12 +29,12 @@ class Dispatch(object):
     def handle_resp(self, nas, data, resp):
         def format_response(resp_msg, user_msg):
             resp_list = []
-            if nas.format == 'text':
+            if nas.p_json:
+                resp_list.append(json.dumps(resp_msg))
+            else:
                 resp_list.append(user_msg)
                 for k, v in resp_msg.iteritems():
                     resp_list.append("{0}: {1}".format(k, v))
-            if nas.format == 'json':
-                resp_list.append(json.dumps(resp_msg))
             return resp_list
 
         resp_msg = self.get_resp_dict(resp)
@@ -43,19 +43,19 @@ class Dispatch(object):
             return 1, format_response(resp_msg,
                                     "http_status: 404 (file not found)")
         elif resp.status_code == 204:
-            if nas.format == 'text':
-                return 0, ["http_status: 204 (request fulfilled)"]
-            else:
+            if nas.p_json:
                 return 0, [json.dumps(resp_msg)]
+            else:
+                return 0, ["http_status: 204 (request fulfilled)"]
         elif resp.status_code == 500:
             resp_list = [_("SERVER ERROR! (Please email this output to a "
                             "code monkey)")]
             return self.error_out(data, resp, resp_list=resp_list)
         elif resp.status_code == 400:
             # Bad Request
-            if nas.format == 'json':
+            if nas.p_json:
                 return 1, [json.dumps(resp_msg)]
-            elif nas.format == 'text':
+            else:
                 if 'error_messages' in resp_msg:
                     return self.get_errors(resp_msg['error_messages'])
                 else:
