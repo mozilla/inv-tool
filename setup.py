5a9ffb0e86bb7988be73e73ba0edbd955b65c34a
uberj <uberj@onid.orst.edu>
2012-11-29 11:47:47 -0800

    moving stuff around

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='Mozilla Inventory Tool',
+    name='invtool',
     version='0.1.0',
     author='Jacques Uber',
     author_email='juber@mozilla.com',
@@ -10,9 +10,6 @@ setup(
     url='https://github.com/uberj/inv-tool',
     license='LICENSE.txt',
     description='An interface to inventory',
-    long_description=open('README.rst').read(),
-    install_requires=[
-        open('requirements.txt').read().strip('\n').replace('\n',',')
-    ],
-    data_files=[('/etc', ['etc/invtool.conf'])],
+    data_files=[('/etc', ['etc/invtool.conf']),
+                ('/usr/local/share/man/man1/', ['docs/man1/invtool.1.gz'])]
 )
