fdc9780a967598327f5b445f4d5306d4b4a197ee
uberj <uberj@onid.orst.edu>
2013-03-20 14:26:07 -0700

    moved stuff around

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version='0.1.0',
     author='Jacques Uber',
     author_email='juber@mozilla.com',
-    packages=['invtool', 'invtool.tests'],
+    packages=['invtool', 'invtool.tests', 'invtool.lib'],
     package_dir={'invtool': 'invtool'},
-    package_data={'invtool': ['invtool/*', 'invtool/*/*.py']},
+    package_data={
+        'invtool': ['invtool/*', 'invtool/*/*/*.py', 'invtool/*/*.py']
+    },
     scripts=['bin/invtool'],
     url='https://github.com/uberj/inv-tool',
     license='LICENSE.txt',
