d3f32c017555686b2d3b0d3af89ae777a8c375a4
uberj <uberj@onid.orst.edu>
2013-08-20 07:32:02 -0700

    add a new line after output and introduce the --pk-only flag

--- invtool/main.py
+++ invtool/main.py
@@ -1,4 +1,5 @@
 import argparse
+import simplejson as json
 
 enabled_dispatches = [
     'invtool.dns_dispatch',
@@ -35,6 +36,11 @@ def main(args):
         '--debug', default=False, dest='DEBUG', action='store_true',
         help="Print stuff"
     )
+    format_group.add_argument(
+        '--pk-only', default=False, dest='p_pk_only', action='store_true',
+        help="If an object was just update/created print the primary key"
+        "of that object otherwise print nothing. No new line is printed."
+    )
     base_parser = inv_parser.add_subparsers(dest='dtype')
 
     # Build parsers. Parses should register arguments.
@@ -42,7 +48,15 @@ def main(args):
         d.build_parser(base_parser)
 
     nas = inv_parser.parse_args(args[1:])
+    if nas.p_pk_only:
+        nas.p_json = True
     resp_code, resp_list = dispatch(nas)
     if not nas.p_silent and resp_list:
-        print '\n'.join(resp_list),
+        if nas.p_pk_only:
+            ret_json = json.loads('\n'.join(resp_list))
+            if 'pk' in ret_json:
+                print ret_json['pk'],
+        else:
+            print '\n'.join(resp_list)
+            print
     return resp_code
